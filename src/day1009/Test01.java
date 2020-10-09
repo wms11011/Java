c5f87044ec0485bace7909ac5406f4264cbcf1d9
Nagisawms <635179224@qq.com>
2020-10-09 10:04:32 +0800

    修改读者登录验证

---
+++ src/day1009/Test01.java
@@ -0,0 +1,15 @@
+package day1009;
+
+/**
+ * @author 王旻爽
+ * 2020/10/9
+ * @ClassName Test01.java
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        short i = 1;
+        i += 1;
+        System.out.println(i);
+    }
+}
