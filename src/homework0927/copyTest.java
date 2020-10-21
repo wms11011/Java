c53fad7b4e8a5dd2d85ae1fabf42814a65b26244
Nagisawms <635179224@qq.com>
2020-10-21 20:44:38 +0800

    Java基础

--- src/homework0927/copyTest.java
+++ src/homework0927/copyTest.java
@@ -21,8 +21,6 @@ public class copyTest {
             fis = new FileInputStream("C:\\Users\\wangl\\Desktop\\2020Java视频学习路线图纯源码版\\01-JavaSE基础阶段\\02-老杜2020版Java零基础进阶368集（视频采用JDK13录制）\\文档\\day23数组拷贝内存图.png");
             // 创建输出流
             fos = new FileOutputStream("E:\\ws10\\数组拷贝内存图.png",true);
-
-            InputStream in = System.in;
             // 对文件进行复制
             byte[] bytes = new byte[1024*1024];
             int reanCount = 0;
